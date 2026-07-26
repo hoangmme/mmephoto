8e15ca837de949f53d222708bda809a6860be858
Felix Pham <felix.p@academyxi.com>
2026-07-26 20:24:05 +0700

    fix: Step 4 Swiper displaying all slides due to block/opacity override v=132

--- js/print-layout.js
+++ js/print-layout.js
@@ -4,12 +4,12 @@
 // Independent module for A5 print layout
 // Reads processed images from IndexedDB batch
 
-import { ALL_TEMPLATES, customTemplates, isStaffMode, A5_WIDTH, A5_HEIGHT, PADDING } from './modules/pl-globals.js?v=131';
+import { ALL_TEMPLATES, customTemplates, isStaffMode, A5_WIDTH, A5_HEIGHT, PADDING } from './modules/pl-globals.js?v=132';
 
-import { StateMixin } from './modules/pl-state.js?v=131';
-import { UIMixin } from './modules/pl-ui.js?v=131';
-import { CanvasMixin } from './modules/pl-canvas.js?v=131';
-import { QueueMixin } from './modules/pl-queue.js?v=131';
+import { StateMixin } from './modules/pl-state.js?v=132';
+import { UIMixin } from './modules/pl-ui.js?v=132';
+import { CanvasMixin } from './modules/pl-canvas.js?v=132';
+import { QueueMixin } from './modules/pl-queue.js?v=132';
 
 class PrintLayoutApp {
   constructor() {
