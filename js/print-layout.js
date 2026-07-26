c1bbded149ab8e49ecbc38371d29ad5b92bf7939
Felix Pham <felix.p@academyxi.com>
2026-07-26 19:12:52 +0700

    style: 3 columns product layout, remove max-width, center title & premium cards v=119

--- js/print-layout.js
+++ js/print-layout.js
@@ -4,12 +4,12 @@
 // Independent module for A5 print layout
 // Reads processed images from IndexedDB batch
 
-import { ALL_TEMPLATES, customTemplates, isStaffMode, A5_WIDTH, A5_HEIGHT, PADDING } from './modules/pl-globals.js?v=118';
+import { ALL_TEMPLATES, customTemplates, isStaffMode, A5_WIDTH, A5_HEIGHT, PADDING } from './modules/pl-globals.js?v=119';
 
-import { StateMixin } from './modules/pl-state.js?v=118';
-import { UIMixin } from './modules/pl-ui.js?v=118';
-import { CanvasMixin } from './modules/pl-canvas.js?v=118';
-import { QueueMixin } from './modules/pl-queue.js?v=118';
+import { StateMixin } from './modules/pl-state.js?v=119';
+import { UIMixin } from './modules/pl-ui.js?v=119';
+import { CanvasMixin } from './modules/pl-canvas.js?v=119';
+import { QueueMixin } from './modules/pl-queue.js?v=119';
 
 class PrintLayoutApp {
   constructor() {
