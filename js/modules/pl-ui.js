99a8a4322a279fbea160bcd32ecf1c275cbb2698
hoangmme <hoji@hoangmmes-MacBook-Air.local>
2026-07-28 16:31:25 +0700

    Bump version to 139 to clear cache

--- js/modules/pl-ui.js
+++ js/modules/pl-ui.js
@@ -1,5 +1,5 @@
-import { ALL_TEMPLATES, customTemplates, isStaffMode, setStaffMode, A5_WIDTH, A5_HEIGHT, PADDING } from './pl-globals.js?v=138';
-import { TemplatePicker } from '../components/TemplatePicker.js?v=138';
+import { ALL_TEMPLATES, customTemplates, isStaffMode, setStaffMode, A5_WIDTH, A5_HEIGHT, PADDING } from './pl-globals.js?v=139';
+import { TemplatePicker } from '../components/TemplatePicker.js?v=139';
 
 export const UIMixin = {
   _initLogin() {
