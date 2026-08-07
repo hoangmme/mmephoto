baee9f0ee300ecf2302c0f75a5e2782c475f3dbe
hoangmme <hoji@hoangmmes-MacBook-Air.local>
2026-08-07 18:17:36 +0700

    Implement Chrome File System Access API for 100% offline direct file writing from slot-editor.html without any server

--- js/modules/pl-canvas.js
+++ js/modules/pl-canvas.js
@@ -1,6 +1,6 @@
-import { ALL_TEMPLATES, customTemplates, isStaffMode, setStaffMode, A5_WIDTH, A5_HEIGHT, PADDING } from './pl-globals.js?v=289';
-import { CanvasRenderer } from '../components/CanvasRenderer.js?v=289';
-import { CanvasExporter } from '../components/CanvasExporter.js?v=289';
+import { ALL_TEMPLATES, customTemplates, isStaffMode, setStaffMode, A5_WIDTH, A5_HEIGHT, PADDING } from './pl-globals.js?v=290';
+import { CanvasRenderer } from '../components/CanvasRenderer.js?v=290';
+import { CanvasExporter } from '../components/CanvasExporter.js?v=290';
 
 export const CanvasMixin = {
   _preloadImage(id, url, useThumb = true) {
--- js/modules/pl-ui.js
+++ js/modules/pl-ui.js
@@ -1,8 +1,8 @@
-import { UICoreMixin } from './pl-ui-core.js?v=289';
-import { UIStepsMixin } from './pl-ui-steps.js?v=289';
-import { UIDraftsMixin } from './pl-ui-drafts.js?v=289';
-import { UIInteractionsMixin } from './pl-ui-interactions.js?v=289';
-import { UIMediaMixin } from './pl-ui-media.js?v=289';
+import { UICoreMixin } from './pl-ui-core.js?v=290';
+import { UIStepsMixin } from './pl-ui-steps.js?v=290';
+import { UIDraftsMixin } from './pl-ui-drafts.js?v=290';
+import { UIInteractionsMixin } from './pl-ui-interactions.js?v=290';
+import { UIMediaMixin } from './pl-ui-media.js?v=290';
 
 export const UIMixin = Object.assign(
   {},
